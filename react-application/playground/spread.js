b4df96ff5d76b335c247445a25963e4c0f66dc8f
reddyshekar <reddyshekar519@gmail.com>
2018-04-02 16:27:54 -0500

    first commit

---
+++ react-application/playground/spread.js
@@ -0,0 +1,32 @@
+// function add (a, b) {
+//   return a + b;
+// }
+//
+// console.log(add(3, 1));
+//
+// var toAdd = [9, 5];
+// console.log(add(...toAdd));
+
+// var groupA = ['Jen', 'Cory'];
+// var groupB = ['Vikram'];
+// var final = [...groupB, 3, ...groupA];
+//
+// console.log(final);
+
+var person = ['Shekar', 25];
+var personTwo = ['Jen', 29];
+// Hi Shekar, you are 25
+
+function greet (name, age) {
+  console.log('Hi ' + name + ', you are ' + age);
+}
+greet(...person);
+greet(...personTwo);
+
+var names = ['Mike', 'Ben'];
+var final = ['Shekar', ...names];
+// Hi Shekar
+
+final.forEach(function (name) {
+  console.log('Hi ' + name);
+});
